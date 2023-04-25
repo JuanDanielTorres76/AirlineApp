e17c8c3031896f0c9c8e76536204926f109462c1
Juanma33 <mipus2005@gmail.com>
2023-04-24 19:05:09 -0500

    the implementation of priority queues and the Passenger class are uploaded

---
+++ src/model/IPriorityQueue.java
@@ -0,0 +1,9 @@
+public interface IPriorityQueue<T extends Comparable<T>> {
+    
+    public void add(T element);
+    public T remove();
+    public boolean isEmpty();
+    public int size();
+    public T peek();
+    
+}
